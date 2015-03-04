e4da4803a06c014e8f86882940b98f456fd8366d
David Liebemann <David.Liebemann@gmail.com>
2015-03-04 18:51:49 +0100

    switching to lwjgl 2.9

--- eclipseProject/idontevenknow/src/de/idontevenknow/game/Main.java
+++ eclipseProject/idontevenknow/src/de/idontevenknow/game/Main.java
@@ -1,5 +1,10 @@
 package de.idontevenknow.game;
 
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 public class Main {
-
+	public static void main(String[] args) {
+		
+		
+	}
 }
