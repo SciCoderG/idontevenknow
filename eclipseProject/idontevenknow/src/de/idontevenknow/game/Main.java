6aa66f93b918be39f28dc3158c93971102ee1af2
kevin2751992 <Admin@10.143.107.149>
2015-03-05 00:14:26 +0100

    edited main class added Display

--- eclipseProject/idontevenknow/src/de/idontevenknow/game/Main.java
+++ eclipseProject/idontevenknow/src/de/idontevenknow/game/Main.java
@@ -1,10 +1,29 @@
 package de.idontevenknow.game;
-
+import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.*;
+
 public class Main {
 	public static void main(String[] args) {
+		try{
+			Display.setDisplayMode(new DisplayMode(800,600));
+			Display.setTitle("idontevenknow");
+			Display.create();
+		} 
+		catch(LWJGLException e){
+			System.out.println(e.getMessage());
+			System.exit(1);	
+		}
 		
+		while(!Display.isCloseRequested()){
+			
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			Display.update();
+		}
+	
+		Display.destroy();
+		System.exit(0);
 		
 	}
 }
