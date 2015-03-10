f6efe6c57429173d2c3bb9cc6e8840b30ae683ad
David Liebemann <David.Liebemann@gmail.com>
2015-03-10 18:43:46 +0100

    rendercomponent added color

--- eclipseProject/idontevenknow/src/de/idontevenknow/engine/render/RenderComponent.java
+++ eclipseProject/idontevenknow/src/de/idontevenknow/engine/render/RenderComponent.java
@@ -1,7 +1,19 @@
 package de.idontevenknow.engine.render;
 
+import org.lwjgl.util.Color;
+
 import de.idontevenknow.engine.Component;
 
 public class RenderComponent extends Component{
+	
+	Color color;
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
 
 }
