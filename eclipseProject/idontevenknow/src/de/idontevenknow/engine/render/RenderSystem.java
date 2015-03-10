36cea361a066d4a293c8af0c653ad3959bc7081b
David Liebemann <David.Liebemann@gmail.com>
2015-03-10 18:36:40 +0100

    changed System to register with a state

--- eclipseProject/idontevenknow/src/de/idontevenknow/engine/render/RenderSystem.java
+++ eclipseProject/idontevenknow/src/de/idontevenknow/engine/render/RenderSystem.java
@@ -1,7 +1,14 @@
 package de.idontevenknow.engine.render;
 
 import de.idontevenknow.engine.System;
+import de.idontevenknow.game.state.State;
 
 public class RenderSystem extends System{
+
+	@Override
+	public void register(State state) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
