e041e0bfab6717145cecb1b83cc55563b0bfd68b
David Liebemann <David.Liebemann@gmail.com>
2015-03-10 15:58:24 +0100

    Reordered Main Method, added initialisation of OpenGL

--- eclipseProject/idontevenknow/src/de/idontevenknow/game/Main.java
+++ eclipseProject/idontevenknow/src/de/idontevenknow/game/Main.java
@@ -1,4 +1,5 @@
 package de.idontevenknow.game;
+
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
@@ -6,24 +7,46 @@ import static org.lwjgl.opengl.GL11.*;
 
 public class Main {
 	public static void main(String[] args) {
-		try{
-			Display.setDisplayMode(new DisplayMode(800,600));
-			Display.setTitle("idontevenknow");
-			Display.create();
-		} 
-		catch(LWJGLException e){
-			System.out.println(e.getMessage());
-			System.exit(1);	
-		}
-		
-		while(!Display.isCloseRequested()){
-			
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			Display.update();
-		}
-	
+		initDisplay();
+		initGL();
+		gameLoop();
+		cleanUp();
+	}
+
+	private static void cleanUp() {
 		Display.destroy();
 		System.exit(0);
+	}
+
+	private static void gameLoop() {
+		while (!Display.isCloseRequested()) {
+
+			glClear(GL_COLOR_BUFFER_BIT); // no need for depth buffer bit, no depth needed in 2d
+			
+			Display.update();
+		}
+	}
+
+	private static void initGL() {
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(0, Display.getWidth(), 0,Display.getHeight(), -1, 1); // set the view in the beginning correctly
+		glMatrixMode(GL_MODELVIEW);
 		
+		glClearColor(1, 1, 1, 1);
+		
+		glDisable(GL_DEPTH_TEST); // not needed in 2d
+		
+	}
+
+	private static void initDisplay() {
+		try {
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.setTitle("idontevenknow");
+			Display.create();
+		} catch (LWJGLException e) {
+			System.err.println(e.getMessage());
+			System.exit(1);
+		}
 	}
 }
