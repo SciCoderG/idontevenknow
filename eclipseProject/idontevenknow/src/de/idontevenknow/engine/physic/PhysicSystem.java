4bb4e0c66beee7c7eeeece38aabed67a3b7bde92
DerjenigeWelche <masterfg@gmx.net>
2015-03-10 18:47:03 +0100

    Physics action Methode (Methodenname ist platzhalter)

--- eclipseProject/idontevenknow/src/de/idontevenknow/engine/physic/PhysicSystem.java
+++ eclipseProject/idontevenknow/src/de/idontevenknow/engine/physic/PhysicSystem.java
@@ -1,10 +1,20 @@
 package de.idontevenknow.engine.physic;
 
+import de.idontevenknow.engine.Component;
 import de.idontevenknow.engine.System;
-import de.idontevenknow.game.state.State;
+import de.idontevenknow.game.gameobjects.GameObject;
 
 public class PhysicSystem extends System{
 
+	public void action(){
+	    for(GameObject go : getRegisteredState().getObjects()){
+	        for(Component c : go.getComponents()){
+	            if(c.getClass() == PhysicComponent.class){
+	                
+	            }
+	        }
+	    }
+	}
 	
-    
+	
 }
